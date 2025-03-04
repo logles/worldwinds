b696365f1999039ed7d97b58a3297980f2b46c96
Ellen <ellen.marie003@gmail.com>
2025-03-03 22:29:36 -0700

    draft css file and associated vite.config modifications to implement tailwindcss and daisyui

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   server: {
     port: 3000,
     open: true,
